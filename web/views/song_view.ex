e6c3938a48894d7f2cc41a029d81726c1fd2e130
Alexander <alxndr+github@gmail.com>
2017-12-03 12:41:40 -0800

    Song page lists Set nicely

--- web/views/song_view.ex
+++ web/views/song_view.ex
@@ -1,6 +1,7 @@
 defmodule JradNet.SongView do
   use JradNet.Web, :view
   alias JradNet.{
+    Set,
     Show,
     Song,
     Venue,
@@ -11,7 +12,9 @@ defmodule JradNet.SongView do
   def title("edit.html", %{conn: %{assigns: %{song: song}}}), do: "Editing: #{Song.readable_name(song)}"
   def title(_, _), do: nil
 
-  def friendly_name(venue), do: Venue.friendly_name(venue)
+  def venue_friendly_name(venue), do: Venue.friendly_name(venue)
+
+  def set_friendly_name(set), do: Set.pretty_name(set.which)
 
   def readable_date(show), do: Show.readable_date(show)
 
