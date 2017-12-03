d81b299f22ab81b69e70f361f39428cdc8b466a9
Alexander <alxndr+github@gmail.com>
2017-12-03 13:58:52 -0800

    extract function into ShowView

--- web/views/show_view.ex
+++ web/views/show_view.ex
@@ -2,9 +2,14 @@ defmodule JradNet.ShowView do
   use JradNet.Web, :view
   alias JradNet.{
     Show,
+    Venue,
   }
 
   def readable_date(show), do: Show.readable_date(show)
+
+  def selected_venue_id(%{"id" => venue_id}), do: venue_id
+  def selected_venue_id(nil), do: ""
+  def selected_venue_id(_venue), do: ""
 
   def title("index.html", _), do: "Shows"
   def title("new.html", _), do: "Add new show"
