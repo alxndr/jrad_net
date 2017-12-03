ab82e31c0e97b3c7710a2270e81c91ecf8e5cca6
Alexander <alxndr+github@gmail.com>
2017-12-03 00:07:32 -0800

    Song edit
    [#1]

--- web/controllers/song_controller.ex
+++ web/controllers/song_controller.ex
@@ -12,6 +12,12 @@ defmodule JradNet.SongController do
   def show(conn, %{"id" => id}) do
     song = Song.get_with_shows(id)
     performances = song.song_performances
-    render conn, "show.html", song: song, performances: performances
+    render conn, "show.html", song: song, performances: performances || []
+  end
+
+  def edit(conn, %{"id" => id}) do
+    song = Song.get(id)
+    changeset = Song.changeset(song)
+    render conn, "edit.html", song: song, changeset: changeset
   end
 end
--- web/views/song_view.ex
+++ web/views/song_view.ex
@@ -7,6 +7,7 @@ defmodule JradNet.SongView do
 
   def title("index.html", _), do: "Songs"
   def title("show.html", %{conn: %{assigns: %{song: song}}}), do: Song.readable_name(song)
+  def title("edit.html", %{conn: %{assigns: %{song: song}}}), do: "Editing: #{Song.readable_name(song)}"
   def title(_, _), do: nil
 
   def friendly_name(show), do: Show.friendly_name(show)
