45b20e653440bd894e117389968b952ebc1e69e9
Alexander <alxndr+github@gmail.com>
2020-06-25 18:35:35 -0700

    add Brave rewards verification file

--- lib/jrad_net/endpoint.ex
+++ lib/jrad_net/endpoint.ex
@@ -9,7 +9,7 @@ defmodule JradNet.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :jrad_net, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known/brave-rewards-verification.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
